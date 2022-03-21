e89a264edd10e18794252335c1b1adaa23e6efd9
17luisasc <luisasc@ua.pt>
2022-03-21 12:56:30 +0000

    version2 - vAvg.h

--- vAvg.h
+++ vAvg.h
@@ -1,10 +1,40 @@
-/*
-* This is vAvg.h
-* Version is: 1 
-*/ 
+/** @file vAvg.h
+ * @brief Calculates the average of an array. 
+ *
+ * Function that calculates the average of the values of an array. * 
+ * 
+ * @author Ana Luísa Coelho
+ * @date 21 March 2022
+ * @bug No known bugs yet.
+/* 
+ * This is vAvg.h
+ * Version is: 2 
+*/
 
-#ifndef _VAVG_H
+#ifndef _VAVG_H 
 #define _VAVG_H
+
+/**
+ * @brief Brief decription of function1 prototype.
+ *
+ * Here it goes the long description of fucntion 1.
+ * Description of what the function does. Usually this part describes 
+ * whatr the fiunction does and its interface, namelly input 
+ * arguments and return value.
+ * Verbatim text can also be used:
+ * @verbatim 
+ * This is my email:luisasc@ua.pt
+ * @endverbatim
+ * You can also include an example of usage:
+ * @code
+ * res = function1(param1, param2);
+ * printf("res=%d\n",res);
+ * @endcode
+ * You can uset HTML tags e.g. to make <b>bold text</b>
+ * @param arg1 Description of the first parameter of the function.
+ * @param arg2 Description of the second parameter of the function.
+ * @return Describe the function return value 
+ */
 
 void vAvg(void);
 #endif
